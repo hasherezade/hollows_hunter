c676242025636bfdac412600d0542e287eab058f
hasherezade <hasherezade@gmail.com>
2020-03-06 17:50:31 +0100

    [FEATURE] Added more colors to term_util

--- term_util.h
+++ term_util.h
@@ -2,8 +2,22 @@
 
 #include <iostream>
 
-#define RED_ON_BLACK 0xC
-#define YELLOW_ON_BLACK 0xE
+#define BLACK 0
+#define DARK_RED 0x4
+#define SILVER 0x7
+#define GRAY 0x8
+#define BLUE 0x9
+#define LIME 0xA
+#define CYAN 0xB
+#define RED 0xC
+#define MAGENTA 0xD
+#define YELLOW 0xE
+#define WHITE 0xF
+
+#define MAKE_COLOR(fg_color, bg_color) (fg_color | (bg_color << 4));
+
+#define YELLOW_ON_BLACK MAKE_COLOR(YELLOW, BLACK)
+#define RED_ON_BLACK MAKE_COLOR(RED, BLACK)
 
 void set_color(int color);
 
