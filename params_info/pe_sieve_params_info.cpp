9ed5d3d363f51ccd4fd67ca19e8602a3ff1cab3b
hasherezade <hasherezade@gmail.com>
2020-07-06 06:40:12 -0700

    [NOBIN] Normalized indentations

--- params_info/pe_sieve_params_info.cpp
+++ params_info/pe_sieve_params_info.cpp
@@ -7,45 +7,45 @@ using namespace pesieve;
 
 std::string translate_dump_mode(const DWORD dump_mode)
 {
-	switch (dump_mode) {
-	case pesieve::PE_DUMP_AUTO:
-		return "autodetect (default)";
-	case pesieve::PE_DUMP_VIRTUAL:
-		return "virtual (as it is in the memory, no unmapping)";
-	case pesieve::PE_DUMP_UNMAP:
-		return "unmapped (converted to raw using sections' raw headers)";
-	case pesieve::PE_DUMP_REALIGN:
-		return "realigned raw (converted raw format to be the same as virtual)";
-	}
-	return "undefined";
+    switch (dump_mode) {
+    case pesieve::PE_DUMP_AUTO:
+        return "autodetect (default)";
+    case pesieve::PE_DUMP_VIRTUAL:
+        return "virtual (as it is in the memory, no unmapping)";
+    case pesieve::PE_DUMP_UNMAP:
+        return "unmapped (converted to raw using sections' raw headers)";
+    case pesieve::PE_DUMP_REALIGN:
+        return "realigned raw (converted raw format to be the same as virtual)";
+    }
+    return "undefined";
 }
 
 std::string translate_out_filter(const pesieve::t_output_filter o_filter)
 {
-	switch (o_filter) {
-	case pesieve::OUT_FULL:
-		return "no filter: dump everything (default)";
-	case pesieve::OUT_NO_DUMPS:
-		return "don't dump the modified PEs, but save the report";
-	case pesieve::OUT_NO_DIR:
-		return "don't dump any files";
-	}
-	return "undefined";
+    switch (o_filter) {
+    case pesieve::OUT_FULL:
+        return "no filter: dump everything (default)";
+    case pesieve::OUT_NO_DUMPS:
+        return "don't dump the modified PEs, but save the report";
+    case pesieve::OUT_NO_DIR:
+        return "don't dump any files";
+    }
+    return "undefined";
 }
 
 std::string translate_imprec_mode(const pesieve::t_imprec_mode imprec_mode)
 {
-	switch (imprec_mode) {
-	case pesieve::PE_IMPREC_NONE:
-		return "none: do not recover imports (default)";
-	case pesieve::PE_IMPREC_AUTO:
-		return "try to autodetect the most suitable mode";
-	case pesieve::PE_IMPREC_UNERASE:
-		return "recover erased parts of the partialy damaged ImportTable";
-	case pesieve::PE_IMPREC_REBUILD:
-		return "build the ImportTable from the scratch, basing on the found IAT(s)";
-	}
-	return "undefined";
+    switch (imprec_mode) {
+    case pesieve::PE_IMPREC_NONE:
+        return "none: do not recover imports (default)";
+    case pesieve::PE_IMPREC_AUTO:
+        return "try to autodetect the most suitable mode";
+    case pesieve::PE_IMPREC_UNERASE:
+        return "recover erased parts of the partialy damaged ImportTable";
+    case pesieve::PE_IMPREC_REBUILD:
+        return "build the ImportTable from the scratch, basing on the found IAT(s)";
+    }
+    return "undefined";
 }
 
 std::string translate_iat_scan_mode(const pesieve::t_iat_scan_mode mode)
