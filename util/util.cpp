7f6eea548532e6a52208c6ee461e131bedbecbf1
hasherezade <hasherezade@gmail.com>
2021-04-11 17:34:09 +0200

    [REFACT] Convert parameters to numbers using string stream

--- util/util.cpp
+++ util/util.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cctype>
+#include <sstream>
 
 size_t strip_to_list(IN std::string s, IN std::string delim, OUT std::set<std::string> &elements_list)
 {
@@ -45,6 +46,17 @@ bool is_dec(const char *buf, size_t len)
     return true;
 }
 
+bool is_cstr_equal(char const *a, char const *b, const size_t max_len)
+{
+    for (size_t i = 0; i < max_len; ++i) {
+        if (tolower(a[i]) != tolower(b[i])) {
+            return false;
+        }
+        if (tolower(a[i]) == '\0') break;
+    }
+    return true;
+}
+
 long get_number(const char *my_buf)
 {
     const char hex_pattern[] = "0x";
@@ -56,16 +68,20 @@ long get_number(const char *my_buf)
     long out = 0;
     const size_t min_length = 1; //tolerate number with at least 1 character is fine
     if (len > hex_pattern_len) {
-        if (strncmp(my_buf, hex_pattern, hex_pattern_len) == 0) {
+        if (is_cstr_equal(my_buf, hex_pattern, hex_pattern_len)) {
             if (!is_hex(my_buf + hex_pattern_len, min_length)) return 0;
 
-            out = std::stoul(my_buf, nullptr, 16);
+            std::stringstream ss;
+            ss << std::hex << my_buf;
+            ss >> out;
             return out;
         }
     }
     if (!is_dec(my_buf, min_length)) return 0;
 
-    out = std::stoul(my_buf, nullptr, 10);
+    std::stringstream ss;
+    ss << std::dec << my_buf;
+    ss >> out;
     return out;
 }
 
@@ -91,7 +107,6 @@ std::string& trim(std::string& str, const std::string& chars)
 }
 
 //
-
 std::string& str_to_lower(std::string& str)
 {
     std::transform(str.begin(), str.end(), str.begin(), tolower);
