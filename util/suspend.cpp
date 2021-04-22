c6a1381fd7eca5ba3abb5f426d9a8e7b3d179328
hasherezade <hasherezade@gmail.com>
2021-04-22 19:47:40 +0200

    [REFACT] Small cleanup in the function: GetParentProcessID

--- util/suspend.cpp
+++ util/suspend.cpp
@@ -49,6 +49,10 @@ bool is_process_associated(DWORD remote_pid)
     DWORD my_parent = GetParentProcessID(my_pid);
     DWORD remote_parent = GetParentProcessID(remote_pid);
 
+    if (my_parent == INVALID_PID || remote_parent == INVALID_PID) {
+        return false;
+    }
+
     bool is_my_child = remote_parent == my_pid;
     bool is_my_parent = my_parent == remote_pid;
     bool is_sibling = my_parent == remote_parent;
@@ -62,7 +66,7 @@ bool is_process_associated(DWORD remote_pid)
 DWORD GetParentProcessID(DWORD dwPID)
 {
     NTSTATUS ntStatus;
-    DWORD dwParentPID = 0xffffffff;
+    DWORD dwParentPID = INVALID_PID;
     HANDLE hProcess;
     PROCESS_BASIC_INFORMATION pbi;
     ULONG ulRetLen;
@@ -72,7 +76,9 @@ DWORD GetParentProcessID(DWORD dwPID)
 
     FPTR_NtQueryInformationProcess NtQueryInformationProcess
         = (FPTR_NtQueryInformationProcess)GetProcAddress(GetModuleHandleA("ntdll"), "NtQueryInformationProcess");
-
+    if (!NtQueryInformationProcess) {
+        return INVALID_PID;
+    }
     //  get process handle
     hProcess = OpenProcess(PROCESS_QUERY_INFORMATION,
         FALSE,
@@ -80,7 +86,8 @@ DWORD GetParentProcessID(DWORD dwPID)
     );
     //  could fail due to invalid PID or insufficiant privileges
     if (!hProcess)
-        return  (0xffffffff);
+        return INVALID_PID;
+
     //  gather information
     ntStatus = NtQueryInformationProcess(hProcess,
         ProcessBasicInformation,
@@ -89,8 +96,8 @@ DWORD GetParentProcessID(DWORD dwPID)
         &ulRetLen
     );
     //  copy PID on success
-    if (!ntStatus)
+    if (ntStatus == S_OK)
         dwParentPID = (DWORD)pbi.InheritedFromUniqueProcessId;
     CloseHandle(hProcess);
-    return  (dwParentPID);
+    return dwParentPID;
 }
--- util/suspend.h
+++ util/suspend.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <windows.h>
 
+#define INVALID_PID (-1)
+
 bool suspend_process(DWORD processId);
 
 bool resume_process(DWORD processId);
 
+// do the process with the given PID belongs to the process tree of the current process (parent/child/sibling)
 bool is_process_associated(DWORD processId);
 
+// retruns the Parent Process PID of the process with the given PID, or INVALID_PID if retrieving it was impossible
 DWORD GetParentProcessID(DWORD dwPID);
