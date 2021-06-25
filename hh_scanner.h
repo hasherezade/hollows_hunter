cbb65d0cc6ec33f3bf543380f5da781c0ff193bd
hasherezade <hasherezade@gmail.com>
2021-06-25 18:44:53 +0200

    [FEATURE] Print all processes names. Refactored enum using CreateToolhelp32Snapshot

--- hh_scanner.h
+++ hh_scanner.h
@@ -4,12 +4,24 @@
 #include <psapi.h>
 #pragma comment(lib,"psapi.lib")
 
+#include<TlHelp32.h>
+
+#include <string>
 #include <vector>
+#include <set>
 
 #include "hh_params.h"
 #include "hh_report.h"
 
 #define PARAM_LIST_SEPARATOR ';'
+
+typedef enum single_status {
+    SSCAN_ERROR1 = (-2),
+    SSCAN_ERROR0 = (-1),
+    SSCAN_NOT_MATCH = 0,
+    SSCAN_IGNORED = 1,
+    SSCAN_SUCCESS = 2
+} t_single_scan_status;
 
 class HHScanner {
 public:
@@ -22,7 +34,13 @@ public:
     void summarizeScan(HHScanReport *hh_report);
 
 protected:
+    void printScanRoundStats(size_t found, size_t ignored_count);
+    size_t scanProcesses(HHScanReport &my_report);
+    void printSingleReport(pesieve::t_report& report);
+
+    t_single_scan_status scanNextProcess(DWORD pid, char* image_buf, HHScanReport &report);
     void initOutDir(time_t scan_time, pesieve::t_params &pesieve_args);
+    void initScanData();
 
     t_hh_params &hh_args;
     std::string outDir;
@@ -30,5 +48,10 @@ protected:
     // time when HollowsHunter was initialized
     time_t initTime;
     bool isScannerWow64;
+
+    // data
+    std::set<std::string> names_list;
+    std::set<std::string> pids_list;
+    std::set<std::string> ignored_names_list;
 };
 
--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 #include "util/process_privilege.h"
 #include "util/strings_util.h"
 
-#define VERSION "0.2.9.7"
+#define VERSION "0.2.9.8"
 
 using namespace hhunter::util;
 
