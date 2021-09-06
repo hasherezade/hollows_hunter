de31d4b0dc83ff0b332aa04003f2bdd21dd6c6f6
hasherezade <hasherezade@gmail.com>
2021-09-06 19:46:00 +0200

    [FEATURE] Added char codes for dump options

--- params_info/pe_sieve_params_info.h
+++ params_info/pe_sieve_params_info.h
@@ -5,14 +5,12 @@
 
 std::string translate_dump_mode(const DWORD dump_mode);
 std::string translate_out_filter(const pesieve::t_output_filter o_filter);
-std::string translate_modules_filter(DWORD m_filter);
 std::string translate_imprec_mode(const pesieve::t_imprec_mode imprec_mode);
 std::string translate_iat_scan_mode(const pesieve::t_iat_scan_mode mode);
 std::string translate_dotnet_policy(const pesieve::t_dotnet_policy &mode);
 std::string translate_json_level(const pesieve::t_json_level &mode);
 
-pesieve::t_imprec_mode normalize_imprec_mode(size_t mode_id);
-pesieve::t_dump_mode normalize_dump_mode(size_t mode_id);
-
 std::string translate_data_mode(const pesieve::t_data_scan_mode &mode);
-pesieve::t_json_level normalize_json_level(size_t mode_id);
+
+std::string dump_mode_to_id(const DWORD dump_mode);
+std::string imprec_mode_to_id(const pesieve::t_imprec_mode imprec_mode);
