a47829cee8767df554b939fa2061312be840c54c
hasherezade <hasherezade@gmail.com>
2021-12-23 08:30:14 -0800

    [FEATURE] Updated pe-sieve. Added support for new ImpRec modes

--- params_info/pe_sieve_params_info.cpp
+++ params_info/pe_sieve_params_info.cpp
@@ -39,8 +39,12 @@ std::string translate_imprec_mode(const pesieve::t_imprec_mode imprec_mode)
         return "try to autodetect the most suitable mode";
     case pesieve::PE_IMPREC_UNERASE:
         return "recover erased parts of the partialy damaged ImportTable";
-    case pesieve::PE_IMPREC_REBUILD:
-        return "build the ImportTable from scratch, basing on the found IATs";
+    case pesieve::PE_IMPREC_REBUILD0:
+        return "build the ImportTable from scratch, basing on the found IATs:\n\t        use only terminated blocks (restrictive mode)";
+    case pesieve::PE_IMPREC_REBUILD1:
+        return "build the ImportTable from scratch, basing on the found IATs:\n\t        use terminated blocks, or blocks with more than 1 thunk";
+    case pesieve::PE_IMPREC_REBUILD2:
+        return "build the ImportTable from scratch, basing on the found IATs:\n\t        use terminated blocks: use all found blocks (aggressive mode)";
     }
     return "undefined";
 }
@@ -133,8 +137,12 @@ std::string imprec_mode_to_id(const pesieve::t_imprec_mode imprec_mode)
         return "A";
     case pesieve::PE_IMPREC_UNERASE:
         return "U";
-    case pesieve::PE_IMPREC_REBUILD:
-        return "R";
+    case pesieve::PE_IMPREC_REBUILD0:
+        return "R0";
+    case pesieve::PE_IMPREC_REBUILD1:
+        return "R1";
+    case pesieve::PE_IMPREC_REBUILD2:
+        return "R2";
     }
     return "N";
 }
