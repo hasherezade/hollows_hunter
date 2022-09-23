f9db03376e321677351eb779f9e56792c5bc1d00
hasherezade <hasherezade@users.noreply.github.com>
2022-09-23 22:31:43 +0000

    deploy: a2d04bdab93622b6213d7e72d112d00ceea7d6a5

--- search/all_6.js
+++ search/all_6.js
@@ -4,6 +4,6 @@ var searchData=
   ['get_5fcurrent_5fcolor_1',['get_current_color',['../term__util_8cpp.html#aff4c0279a81f579add3c3b1e9f208ee9',1,'get_current_color(int descriptor, WORD &amp;color):&#160;term_util.cpp'],['../term__util_8h.html#aff4c0279a81f579add3c3b1e9f208ee9',1,'get_current_color(int descriptor, WORD &amp;color):&#160;term_util.cpp']]],
   ['get_5fprocess_5finfo_2',['get_process_info',['../namespaceprocess__util.html#a4a258bf9fcaf9b1f5b6419f4e6a5d1e1',1,'process_util']]],
   ['getparentprocessid_3',['GetParentProcessID',['../suspend_8cpp.html#a6844401d82ea5410225fae1dd2f33365',1,'GetParentProcessID(DWORD dwPID):&#160;suspend.cpp'],['../suspend_8h.html#a6844401d82ea5410225fae1dd2f33365',1,'GetParentProcessID(DWORD dwPID):&#160;suspend.cpp']]],
-  ['getscantime_4',['getScanTime',['../class_h_h_scan_report.html#a529a910b70a7476252d9be567890f667',1,'HHScanReport']]],
+  ['getscantime_4',['getScanTime',['../class_h_h_scan_report.html#ae662be540e84fe79516c5fc79c6d9999',1,'HHScanReport']]],
   ['gray_5',['GRAY',['../term__util_8h.html#ae5f70677050eecd8909e0248e07b9e73',1,'term_util.h']]]
 ];
