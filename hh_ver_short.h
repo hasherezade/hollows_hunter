fd85293224673ebeb7766737042cb5113a9186d9
hasherezade <hasherezade@gmail.com>
2023-05-14 13:08:16 -0700

    [VERSION] 0.3.6

--- hh_ver_short.h
+++ hh_ver_short.h
@@ -2,7 +2,7 @@
 
 #define HH_MAJOR_VERSION 0
 #define HH_MINOR_VERSION 3
-#define HH_MICRO_VERSION 5
+#define HH_MICRO_VERSION 6
 #define HH_PATCH_VERSION 0
 
-#define HH_VERSION_STR	"0.3.5"
+#define HH_VERSION_STR	"0.3.6"
