992fe763dcc723b7722472e82ecf2c5af2697a98
hasherezade <hasherezade@gmail.com>
2024-02-24 11:23:43 -0800

    [VERSION] 0.3.9

--- hh_ver_short.h
+++ hh_ver_short.h
@@ -2,7 +2,7 @@
 
 #define HH_MAJOR_VERSION 0
 #define HH_MINOR_VERSION 3
-#define HH_MICRO_VERSION 8
-#define HH_PATCH_VERSION 2
+#define HH_MICRO_VERSION 9
+#define HH_PATCH_VERSION 0
 
-#define HH_VERSION_STR	"0.3.8.2"
+#define HH_VERSION_STR	"0.3.9"
