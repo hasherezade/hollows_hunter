aff2230a73533d46a9e21e133c71aa3d581e46a0
hasherezade <hasherezade@users.noreply.github.com>
2024-09-25 00:17:12 +0000

    deploy: 6cfd370a1231b07bc1aab04a210e4b5e5187e821

--- search/all_c.js
+++ search/all_c.js
@@ -4,6 +4,6 @@ var searchData=
   ['main_1',['main',['../main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.cpp']]],
   ['main_2ecpp_2',['main.cpp',['../main_8cpp.html',1,'']]],
   ['make_5fcolor_3',['MAKE_COLOR',['../term__util_8h.html#a57815732ee1775789c29534c08b0a856',1,'term_util.h']]],
-  ['make_5fdir_5fname_4',['make_dir_name',['../namespacefiles__util.html#a9c2cf1240e4b850893c4e7a0a2ab641b',1,'files_util']]],
+  ['make_5fdir_5fname_4',['make_dir_name',['../namespacefiles__util.html#abf7175154c7107a589ece4a36ef52782',1,'files_util']]],
   ['min_5',['MIN',['../strings__util_8cpp.html#a74e75242132eaabbc1c512488a135926',1,'strings_util.cpp']]]
 ];
--- search/functions_a.js
+++ search/functions_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['main_0',['main',['../main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.cpp']]],
-  ['make_5fdir_5fname_1',['make_dir_name',['../namespacefiles__util.html#a9c2cf1240e4b850893c4e7a0a2ab641b',1,'files_util']]]
+  ['make_5fdir_5fname_1',['make_dir_name',['../namespacefiles__util.html#abf7175154c7107a589ece4a36ef52782',1,'files_util']]]
 ];
