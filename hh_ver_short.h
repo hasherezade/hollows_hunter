dd981e4a415839b93c08cdc676630373766cc262
hasherezade <hasherezade@gmail.com>
2024-12-14 06:41:15 -0800

    [VERSION] 0.4.0. Updated pe-sieve

--- hh_ver_short.h
+++ hh_ver_short.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define HH_MAJOR_VERSION 0
-#define HH_MINOR_VERSION 3
-#define HH_MICRO_VERSION 9
-#define HH_PATCH_VERSION 8
+#define HH_MINOR_VERSION 4
+#define HH_MICRO_VERSION 0
+#define HH_PATCH_VERSION 0
 
-#define HH_VERSION_STR	"0.3.9.8"
+#define HH_VERSION_STR	"0.4.0.0"
