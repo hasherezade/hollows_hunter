f22055213886220ff105e4cf4efb4ec68be181d5
hasherezade <hasherezade@gmail.com>
2025-02-09 13:53:47 -0800

    [VERSION] 0.4.1

--- hh_ver_short.h
+++ hh_ver_short.h
@@ -2,7 +2,7 @@
 
 #define HH_MAJOR_VERSION 0
 #define HH_MINOR_VERSION 4
-#define HH_MICRO_VERSION 0
-#define HH_PATCH_VERSION 2
+#define HH_MICRO_VERSION 1
+#define HH_PATCH_VERSION 0
 
-#define HH_VERSION_STR	"0.4.0.2"
+#define HH_VERSION_STR	"0.4.1"
