003ae79f74ccef2146e8e38e15feb203ef48b91b
hasherezade <hasherezade@gmail.com>
2026-04-14 14:42:33 +0200

    [NOBIN] Updated CMake flags to the new type in the mingw_build script

--- mingw_build.sh
+++ mingw_build.sh
@@ -7,9 +7,7 @@ cmake . \
     -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ \
     -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres \
     -DCMAKE_SYSTEM_NAME=Windows-GNU \
-    -DPESIEVE_AS_DLL=0 \
-    -DPESIEVE_AS_STATIC_LIB=1 \
+    -DHH_PESIEVE_LINK_MODE=STATIC \
     -DLINK_STATICALLY=1
 
 make
-
